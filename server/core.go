ab15b587c952181aef0644215b6dd92e1a76c12d
a.shorokhov <a.shorokhov@vk.team>
2023-11-22 23:22:05 +0300

    chore(kit): remove default stacktrace in logs

--- server/core.go
+++ server/core.go
@@ -2,12 +2,10 @@ package kit
 
 import (
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 func initDefaultZapLogger() *zap.Logger {
 	return zap.Must(zap.NewProduction(
 		zap.AddCaller(),
-		zap.AddStacktrace(zapcore.ErrorLevel),
 	))
 }
